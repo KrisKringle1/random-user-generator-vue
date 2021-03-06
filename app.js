838d7a260c439075123e057ced3154ada2adc7fe
KrisKringle1 <kmringler@gmail.com>
2021-03-05 16:00:29 -0800

    finished demonstration

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg'
         }
+    },
+    methods: {
+        getUser(){
+            axios.get('https://randomuser.me/api')
+            .then(response => {
+                const data = response.data.results
+                this.firstName =  data[0].name.first
+                this.lastName = data[0].name.last
+                this.email = data[0].email
+                this.gender = data[0].gender
+                this.picture = data[0].picture.large
+            })
+           
+        }
     }
 })
 
